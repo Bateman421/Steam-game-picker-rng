64eeb3cdfca53ff9992fc6a4faf256af2344170b
Unknown <kevin_said@outlook.com>
2021-10-25 18:48:53 -0500

    Obtencion de Imagen y ID del juego
    codigo que obtiene el "src" de la imagen de la carta del juego y el ID

--- steamRNG/gamepicker/views.py
+++ steamRNG/gamepicker/views.py
@@ -1,8 +1,8 @@
+import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.opera.options import Options
 from selenium import webdriver
-import time
 from bs4 import BeautifulSoup
 from django.shortcuts import render
 from.forms import SteamIdForm
@@ -20,14 +20,30 @@ def home(request):
             driver = webdriver.Opera(
                 options=options, executable_path=r'C:\Program Files\Opera\80.0.4170.63\operadriver.exe')
             driver.get('https://steamcommunity.com/id/'+text+'/games/?tab=all')
-            # time.sleep(1)
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
             soup = BeautifulSoup(driver.page_source, 'lxml')
-            games_names = soup.select('div.gameListRowItemName.ellipsis')
-            # games_hours = soup.find_all('h5')
-            # games_imgs = soup.find_all('img')
+            games_ids = soup.find_all('div', class_='gameListRow')
+
             driver.close()
-            for i in range(0, len(games_names)):
-                print(games_names[i].text)
+            for i in range(0, len(games_ids)):
+
+                # Sopa y ciclo especifico para obtener el nombre de la bibloteca de juegos
+                name = games_ids[i].select('div.gameListRowItemName.ellipsis')
+                for h in range(0, len(name)):
+                    print(name[h].text)
+
+                # Sopa y ciclo especifico para obtener la fuente de la imagen del juego
+                img = games_ids[i].findAll('img', class_='game_capsule')
+                for j in img:
+                    if j['src'].endswith('.jpg'):
+                        print(j['src'])
+
+                # Imprecion del Id especifico del juego
+                print(games_ids[i].get('id'))
+
+                # print(games_imgs[i])
+
             # print(games_hours)
             # print(games_imgs)
 
