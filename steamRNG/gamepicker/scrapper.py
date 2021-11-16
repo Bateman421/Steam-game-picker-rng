a7bc261fc376fec4ab7f8e9754f8dc98f77fbc4d
Unknown <kevin_said@outlook.com>
2021-11-16 12:38:07 -0600

    Trying to merge...

--- steamRNG/gamepicker/scrapper.py
+++ steamRNG/gamepicker/scrapper.py
@@ -3,17 +3,17 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 
-options = Options()
-options.binary_location = r'C:\Program Files\Opera\80.0.4170.63\Opera.exe'
-
 
 class Scrapper():
     def gameList(steam_id):
 
         # Selenium driver, en teoria solo deveria correr "server side" Cliente final
         # no deberia toparse con esta funcionalidad corriendo en su terminal
-        driver = webdriver.Opera(
-            options=options, executable_path=r'C:\Program Files\Opera\80.0.4170.63\operadriver.exe')
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        driver = webdriver.Chrome(
+            '.\gamepicker\WebDriver\chromedriver',
+            options=options)
         driver.get('https://steamcommunity.com/id/'+steam_id+'/games/?tab=all')
         driver.execute_script(
             "window.scrollTo(0, document.body.scrollHeight);")
